bffd903883b9c68d2b3279b0c0c8cf896cac1d5e
Aleksandr Shavlyuga <a.shavlyuga@itransition.com>
2023-03-13 21:31:34 +0100

    check trigger

--- deploy.groovy
+++ deploy.groovy
@@ -11,13 +11,13 @@ pipeline {
            projectName: 'maven-build',
            filter: '**/*.jar, **/serverless.yml',
            fingerprintArtifacts: true,
-		   flatten: true)
+		   flatten: false)
          sh 'serverless config credentials \
 		   --provider aws \
 		   --key $STAGE_AWS_ACCESS_KEY_ID \
 		   --secret $STAGE_AWS_SECRET_ACCESS_KEY'
 		 sh 'ls'
-		 sh 'cd /lambda'
+		 sh 'cd  /lambda'
 		 sh 'ls'
 		 sh 'serverless deploy --verbose'
 	   }
